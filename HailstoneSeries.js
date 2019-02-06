adb6ed7e442574149e421b7d87608f41629d2e75
Serhii Vekshyn <serhiivekshyn@gmail.com>
2019-02-06 17:33:25 +0200

    ready script

---
+++ HailstoneSeries.js
@@ -0,0 +1,36 @@
+/*HailstoneSeries*/
+console.log('Hailstone series');
+
+let a0 = 10;
+let a = a0;
+let counter = 0;
+let bufferRow = [a];
+let reiteration = true;
+let addEll = true;
+
+function HailstoneSeries(a,counter) {
+    while (reiteration !== false) {
+        if ((a%2)==0){
+            a = a/2;
+        }else{
+            a = a*3+1;
+        }
+        bufferRow.map((el)=>{
+            if (el == a){
+                reiteration = false;
+                addEll = false;
+            } else {
+            }
+        });
+        if (addEll !== false){
+            ++counter;
+            bufferRow.push(a);
+        };
+    }
+    /*result*/
+    console.log('Stopping tme: f('+a0+')=' + counter);
+    /*if needed to show array*/
+    console.log('Hailstone series is ' + bufferRow);
+}
+
+HailstoneSeries(a,counter);
